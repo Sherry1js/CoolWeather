918bf80561e0e573044e072d2e06f0ae84aee3df
Sherry1js <js19960751@163.com>
2018-09-29 00:20:57 +0800

    完成遍历省市县三级列表的功能

---
+++ app/src/main/java/com/sherry/coolweather/util/HttpUtil.java
@@ -0,0 +1,20 @@
+package com.sherry.coolweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+import static android.R.string.ok;
+
+/**
+ * Author:Sherry
+ * Date:2018/9/28
+ * 作用：
+ */
+
+public class HttpUtil{
+    public static void sendOkHttpRequest(String address,okhttp3.Callback callback) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
